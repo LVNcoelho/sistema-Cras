bd1fed26714bc4eda2d4cad0a9f6d538b4aca83b
Conecta TI <pamelacoelho117@gmail.com>
2026-01-22 11:57:59 -0300

    ajuste de importação

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { Search, Users, FileText, Wifi, WifiOff, PlusCircle } from 'lucide-react';
+import { supabase } from '../lib/supabase';
 
 export default function Dashboard() {
   const [isOnline, setIsOnline] = useState(true);
